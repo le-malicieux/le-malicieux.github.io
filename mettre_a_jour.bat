3448fd3e405c1b69ee990f9477e897412b175faf
le-malicieux <zach.bisanti@gmail.com>
2026-04-28 18:12:56 +0200

    Add files via upload

---
+++ mettre_a_jour.bat
@@ -0,0 +1,14 @@
+@echo off
+echo ================================================
+echo     Mise a jour du site Le Malicieux
+echo ================================================
+echo.
+
+python update_csv.py
+
+echo.
+echo ================================================
+echo Pense a push sur GitHub !
+echo ================================================
+echo.
+pause
